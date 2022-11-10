3fa77106e13654d7fb2bd5f15a00beefa202dac3
Felipegadelha <felipegadelha062@gmail.com>
2022-11-10 20:49:29 -0300

    eefefef

--- desafio_05.js
+++ desafio_05.js
@@ -7,10 +7,19 @@
 
             listaDePessoas.forEach(function(pessoa){
                 console.log(pessoa.name, pessoa.email)
+                const li = document.createElement('li')
+                li.innerHTML = `NOME: ${listaDePessoas.name}`
+              document.getElementById('exec6').appendChild('li')
             })
 
+        
+            // const li = document.createElement('li')
+            // li.innerHTML = `NOME: ${listaDePessoas.name}`
+            // document.getElementById('exec6').appendChild('li')
+
         }) 
-  }
+    }
+  
   )
 
 
