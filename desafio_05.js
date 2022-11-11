b799e48c3117871f99ed5d3362ca23c7936d4c14
Felipegadelha <felipegadelha062@gmail.com>
2022-11-11 19:31:52 -0300

    ooi

--- desafio_05.js
+++ desafio_05.js
@@ -5,7 +5,7 @@ fetch('http://jsonplaceholder.typicode.com/users')
   return pessoa.map(function(itens){
 
       const li = document.createElement('li')
-      li.innerHTML = `<strong><br> NOME:</strong> ${itens.name} <strong><br> Email:<strong> ${itens.email}`
+      li.innerHTML = `<strong><br> NOME:</strong> ${itens.name} <strong><br> Email:</strong> ${itens.email}`
       document.getElementById('exec6').appendChild(li)
 
     })
